de23e9f6d51f808d77dffe163e1f8bdecbeb147a
kreptv <73133240+kreptv@users.noreply.github.com>
2023-04-11 09:23:28 -0500

    push

--- BlinkDetectionUnity/Assets/ModelSenseBlink.cs
+++ BlinkDetectionUnity/Assets/ModelSenseBlink.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class ModelSenseBlink : MonoBehaviour
 {
@@ -14,29 +16,45 @@ public class ModelSenseBlink : MonoBehaviour
 
     private Animator ani;
 
-    void Start()
-    {
+    public GameObject gameOver;
+    public TextMeshProUGUI currentBlinkstxt;
+    public TextMeshProUGUI maxBlinkstxt;
+
+    public Button restart;
+
+    public void Restart(){
+
+      currentBlinks = 0;
+
+      currentBlinkstxt.text = "0"; maxBlinkstxt.text = maxBlinks.ToString();
+      gameOver.SetActive(false);
       model = this.gameObject;
+      model.transform.position = new Vector3(0f,1.04f,-6.69f);
       EyeData.Instance.BlinkEvent.AddListener(ModelMove);
       ani = model.GetComponent<Animator>();
     }
 
+    void Start()
+    {
+
+      restart.GetComponent<Button>();
+      restart.onClick.AddListener(Restart);
+
+      Restart();
+    }
+
     public void ModelMove(){
-      currentBlinks ++;
+      ani.SetTrigger("nextState");
+      currentBlinks ++; currentBlinkstxt.text = currentBlinks.ToString();
       model.transform.position -= new Vector3(0f, 0f, moveSpeed);
       ani.SetBool("gameOver", false);
 
       if (currentBlinks == maxBlinks){
+        gameOver.SetActive(true);
         Debug.Log("Game Over!");
         EyeData.Instance.BlinkEvent.RemoveListener(ModelMove);
         ani.SetBool("gameOver", true);
       }
-      ani.SetTrigger("nextState");
-      ani.ResetTrigger("nextState");
-
-
-
-
     }
 
     // Update is called once per frame
